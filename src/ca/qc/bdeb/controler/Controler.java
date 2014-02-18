5293e81d0ddc9e09f4418a29aa3fdded928f8b01
ChronoIII <samuel@jfzone.net>
2014-02-18 10:35:30 -0500

    nouveau projet
    lest go les amis

---
+++ src/ca/qc/bdeb/controler/Controler.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.qc.bdeb.controler;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Controler {
+    
+    
+    //allo les amis
+    //Salut les amis cest zi :D oui je vois
+    //Salut les amis cest sam
+    //allo sam
+	    
+    
+}
