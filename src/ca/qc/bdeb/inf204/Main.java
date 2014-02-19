bf6f2fd46ca631b7fb8b9ed3848532cab81c4265
ChronoIII <samuel@jfzone.net>
2014-02-19 12:47:17 -0500

    (Main) test

--- src/ca/qc/bdeb/inf204/Main.java
+++ src/ca/qc/bdeb/inf204/Main.java
@@ -5,8 +5,43 @@
 
 package ca.qc.bdeb.inf204;
 
-public class Main {
+import org.newdawn.slick.*;
 
-    public static void main(String[] args) {
+public class Main extends BasicGame{
+
+     static int width = 640;
+    static int height = 480;
+   
+    static boolean fullscreen = true;
+    static boolean showFPS = true;
+    static String title = "game test";
+    static int fpslimit = 40;
+   
+    public Main(String title) {
+        super(title);
+    }
+ 
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+       
+    }
+ 
+    @Override
+    public void update(GameContainer gc, int delta) throws SlickException {
+       
+    }
+ 
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+       
+    }
+   
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new Main(title));
+        app.setDisplayMode(width, height, fullscreen);
+        app.setSmoothDeltas(true);
+        app.setTargetFrameRate(fpslimit);
+        app.setShowFPS(showFPS);
+        app.start();
     }
 }
