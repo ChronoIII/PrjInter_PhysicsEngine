5f57e3b436be4026bac82dc7aa1ac0833ff72b06
ChronoIII <samuel@jfzone.net>
2014-02-24 15:58:41 -0500

    reglage sur prolème que git nous cause

--- src/ca/qc/bdeb/vue/EngineScreen.java
+++ src/ca/qc/bdeb/vue/EngineScreen.java
@@ -16,7 +16,6 @@ import org.newdawn.slick.state.StateBasedGame;
  *
  * @author Samuel
  */
-<<<<<<< HEAD:src/ca/qc/bdeb/views/EngineScreen.java
 public class EngineScreen extends BasicGameState{
 
     public EngineScreen(int state) {
@@ -41,12 +40,5 @@ public class EngineScreen extends BasicGameState{
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
 	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-=======
-public class EngineScreen {
-
-    private Ennemie enn = new Ennemie();
-
-    public EngineScreen() {
->>>>>>> 3429911b92220f898b05e72380d74447d8c76e73:src/ca/qc/bdeb/vue/EngineScreen.java
     }
 }
